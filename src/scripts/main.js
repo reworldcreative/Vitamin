38855ca99fb375002d3173364cfbe8fd22b6bfc1
reworldcreative <reworldcreative@gmail.com>
2023-06-19 16:31:06 +0300

    add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 import * as functions from "./modules/functions.js";
 
 functions.isWebp();
+
+$(document).ready(function () {
+  $(".burger-button").click(function (event) {
+    $(".burger-menu").toggleClass("active");
+  });
+
+  $(".burger-menu .close-button").click(function (event) {
+    $(".burger-menu").toggleClass("active");
+  });
+});
