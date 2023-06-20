c73c2e523987c3ca0b8c4cba3ff8136b581515a1
reworldcreative <reworldcreative@gmail.com>
2023-06-20 18:27:55 +0300

    fix speed loading

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,18 @@ $(document).ready(function () {
     $(".burger-menu").toggleClass("active");
   });
 });
+
+// Завантаження стилів після першого відображення сторінки
+function loadCSS(url) {
+  var link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = url;
+  document.head.appendChild(link);
+}
+
+window.addEventListener("load", function () {
+  loadCSS("https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css");
+  loadCSS(
+    "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
+  );
+});
