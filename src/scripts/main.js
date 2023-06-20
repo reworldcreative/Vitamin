0b6717639a77d44dccad3a9fd6ebe495485b6b37
reworldcreative <reworldcreative@gmail.com>
2023-06-20 18:58:11 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,9 +20,21 @@ function loadCSS(url) {
   document.head.appendChild(link);
 }
 
-window.addEventListener("load", function () {
+window.addEventListener("DOMContentLoaded", function () {
   loadCSS("https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css");
   loadCSS(
     "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
   );
+
+  var jqueryScript = document.createElement("script");
+  jqueryScript.src = "https://code.jquery.com/jquery-3.7.0.min.js";
+  jqueryScript.integrity =
+    "sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=";
+  jqueryScript.crossOrigin = "anonymous";
+  document.body.appendChild(jqueryScript);
+
+  var swiperScript = document.createElement("script");
+  swiperScript.src =
+    "https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js";
+  document.body.appendChild(swiperScript);
 });
