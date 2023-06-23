c515aa8898a192fe7033fb5db57bcf85dcd076a0
reworldcreative <reworldcreative@gmail.com>
2023-06-23 13:58:07 +0300

    speed load optimization

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -37,6 +37,9 @@ window.addEventListener("DOMContentLoaded", function () {
   var swiperScript = document.createElement("script");
   swiperScript.src =
     "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js";
+  var lazyloadxt = document.createElement("script");
+  lazyloadxt.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyloadxt/1.1.0/jquery.lazyloadxt.min.js";
 
   swiperScript.onload = function () {
     if ($(".ChooseProducts__swiper").length > 0) {
@@ -87,6 +90,7 @@ window.addEventListener("DOMContentLoaded", function () {
   };
 
   document.body.appendChild(swiperScript);
+  document.body.appendChild(lazyloadxt);
 });
 
 // var IscheckSize = false;
@@ -116,3 +120,5 @@ checkSize();
 $(window).resize(function () {
   checkSize();
 });
+
+$(window).lazyLoadXT();
